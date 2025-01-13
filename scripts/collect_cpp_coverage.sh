e23c784209af398a249b3b30e2e1e17299a09262
Xiangyu Wang <wxy407827@antgroup.com>
2025-01-13 17:24:39 +0800

    update coverage configuration (#316)
    Signed-off-by: wxy407827 <wxy407827@antgroup.com>

--- scripts/collect_cpp_coverage.sh
+++ scripts/collect_cpp_coverage.sh
@@ -5,7 +5,6 @@ set -x
 
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ROOT_DIR="$( cd "${SCRIPTS_DIR}/../" && pwd )"
-PARENT_DIR="$( cd "${ROOT_DIR}/../" && pwd )"
 
 COVERAGE_DIR="${ROOT_DIR}/coverage"
 if [ -d "${COVERAGE_DIR}" ]; then
@@ -14,25 +13,24 @@ else
     mkdir -p "${COVERAGE_DIR}"
 fi
 
-pushd "${PARENT_DIR}"
 lcov --rc branch_coverage=1 \
      --rc geninfo_unexecuted_blocks=1 \
      --parallel 8 \
-     --directory vsag \
+     --directory . \
      --capture \
-     --substitute "s#${PARENT_DIR}/##g" \
+     --substitute "s#${ROOT_DIR}/##g" \
      --ignore-errors mismatch,mismatch \
      --ignore-errors count,count \
      --output-file ${COVERAGE_DIR}/coverage.info
 lcov --remove ${COVERAGE_DIR}/coverage.info \
      '/usr/*' \
-     'vsag/build/*' \
-     'vsag/tests/*' \
+     'build/*' \
+     'tests/*' \
+     '*/expected.hpp' \
      --ignore-errors inconsistent,inconsistent \
      --output-file ${COVERAGE_DIR}/coverage.info
 lcov --list ${COVERAGE_DIR}/coverage.info \
      --ignore-errors inconsistent,inconsistent
-popd
 
 pushd "${COVERAGE_DIR}"
 coverages=$(ls coverage.info)
