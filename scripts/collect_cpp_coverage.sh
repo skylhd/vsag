c2d39e18139c3a06d7b18b58d5b5c93fffc92be6
Xiangyu Wang <wxy407827@antgroup.com>
2025-01-13 11:08:07 +0800

    use lcov v2.3 in github ci (#311)
    Signed-off-by: Xiangyu Wang <wxyucs@gmail.com>

--- scripts/collect_cpp_coverage.sh
+++ scripts/collect_cpp_coverage.sh
@@ -4,7 +4,8 @@ set -e
 set -x
 
 SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ROOT_DIR="${SCRIPTS_DIR}/../"
+ROOT_DIR="$( cd "${SCRIPTS_DIR}/../" && pwd )"
+PARENT_DIR="$( cd "${ROOT_DIR}/../" && pwd )"
 
 COVERAGE_DIR="${ROOT_DIR}/coverage"
 if [ -d "${COVERAGE_DIR}" ]; then
@@ -13,17 +14,25 @@ else
     mkdir -p "${COVERAGE_DIR}"
 fi
 
+pushd "${PARENT_DIR}"
 lcov --rc branch_coverage=1 \
      --rc geninfo_unexecuted_blocks=1 \
      --parallel 8 \
-     --directory . \
+     --directory vsag \
      --capture \
+     --substitute "s#${PARENT_DIR}/##g" \
      --ignore-errors mismatch,mismatch \
      --ignore-errors count,count \
      --output-file ${COVERAGE_DIR}/coverage.info
-
-lcov --remove ${COVERAGE_DIR}/coverage.info '/usr/*' '*/build/*' '*/vsag/tests/*' --ignore-errors inconsistent,inconsistent --output-file ${COVERAGE_DIR}/coverage.info
-lcov --list ${COVERAGE_DIR}/coverage.info --ignore-errors inconsistent,inconsistent
+lcov --remove ${COVERAGE_DIR}/coverage.info \
+     '/usr/*' \
+     'vsag/build/*' \
+     'vsag/tests/*' \
+     --ignore-errors inconsistent,inconsistent \
+     --output-file ${COVERAGE_DIR}/coverage.info
+lcov --list ${COVERAGE_DIR}/coverage.info \
+     --ignore-errors inconsistent,inconsistent
+popd
 
 pushd "${COVERAGE_DIR}"
 coverages=$(ls coverage.info)
