42d8e42a47e0ad8b221260d97ff9e4946fff0fb4
LHT129 <tianlan.lht@antgroup.com>
2025-02-17 10:45:41 +0800

    add libaio deps for ubuntu and centos (#402)
    Signed-off-by: LHT129 <tianlan.lht@antgroup.com>

--- scripts/deps/install_deps_centos.sh
+++ scripts/deps/install_deps_centos.sh
@@ -1,5 +1,5 @@
 # install python3-devel
-yum install -y gfortran python3-devel
+yum install -y gfortran python3-devel libaio-devel
 
 # install openmp for alibaba clang 11
 yum install -b current -y libomp11-devel libomp11
--- scripts/deps/install_deps_ubuntu.sh
+++ scripts/deps/install_deps_ubuntu.sh
@@ -2,10 +2,10 @@ arch=$(uname -m)
 
 if [[ "$arch" == "x86_64" ]]; then
     echo "Executing apt install for x86_64"
-    apt update && apt install -y gfortran python3-dev libomp-15-dev lcov intel-mkl gcc make cmake g++
+    apt update && apt install -y gfortran python3-dev libomp-15-dev gcc make cmake g++ lcov libaio-dev intel-mkl
 elif [[ "$arch" == "aarch64" ]]; then
     echo "Executing apt install for aarch64"
-    apt update && apt install -y gfortran python3-dev libomp-15-dev gcc make cmake g++ lcov libopenblas-dev
+    apt update && apt install -y gfortran python3-dev libomp-15-dev gcc make cmake g++ lcov libaio-dev libopenblas-dev
 else
     echo "Unknown architecture: $arch"
 fi
